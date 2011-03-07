3c0a3018f29acd3c2213cd26e901c58f038e2b9e
Fredrik Appelberg <fredrik.appelberg@gmail.com>
2011-03-07 14:36:12 +0100

    fix to avoid parse errors if $TERM is empty

--- lib/termsupport.zsh
+++ lib/termsupport.zsh
@@ -3,12 +3,11 @@
 #Fully support screen, iterm, and probably most modern xterm and rxvt
 #Limited support for Apple Terminal (Terminal can't set window or tab separately)
 function title {
-  [ "$DISABLE_AUTO_TITLE" != "true" ] || return
-  if [[ "$TERM" == screen* ]]; then 
-    print -Pn "\ek$1:q\e\\" #set screen hardstatus, usually truncated at 20 chars
-  elif [[ "$TERM" == xterm* ]] || [[ $TERM == rxvt* ]] || [[ "$TERM_PROGRAM" == "iTerm.app" ]]; then
-    print -Pn "\e]2;$2:q\a" #set window name
-    print -Pn "\e]1;$1:q\a" #set icon (=tab) name (will override window name on broken terminal)
+  if [[ "$TERM" == "screen" ]]; then 
+    print -Pn "\ek$1\e\\" #set screen hardstatus, usually truncated at 20 chars
+  elif [[ ("$TERM" =~ "^xterm") ]] || [[ ("$TERM" == "rxvt") ]] || [[ "$TERM_PROGRAM" == "iTerm.app" ]]; then
+    print -Pn "\e]2;$2\a" #set window name
+    print -Pn "\e]1;$1\a" #set icon (=tab) name (will override window name on broken terminal)
   fi
 }
 
