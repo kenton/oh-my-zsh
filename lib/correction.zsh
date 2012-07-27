312b4e446f392e569715294d0ea5b03def81fda9
Morgan Larosa <chaos95@gmail.com>
2012-07-28 08:25:15 +1000

    Add a configuration option to disable autocorrect

--- lib/correction.zsh
+++ lib/correction.zsh
@@ -1,23 +1,14 @@
-setopt correct_all
+if [[ "$DISABLE_CORRECTION" == "true" ]]; then
+  return
+else
+  setopt correct_all
 
-alias man='nocorrect man'
-alias mv='nocorrect mv'
-alias mysql='nocorrect mysql'
-alias mkdir='nocorrect mkdir'
-alias gist='nocorrect gist'
-alias heroku='nocorrect heroku'
-alias ebuild='nocorrect ebuild'
-alias hpodder='nocorrect hpodder'
-alias feature/ondemand='nocorrect feature/ondemand'
-alias feature/ondemand-geogrid='nocorrect feature/ondemand-geogrid'
-alias feature/private-rates='nocorrect feature/private-rates'
-alias feature/promo-codes='nocorrect feature/promo-codes'
-alias feature/restrict-editing-of-subscription-fields='nocorrect feature/restrict-editing-of-subscription-fields'
-alias feature/persistent-promo-codes='nocorrect feature/persistent-promo-codes'
-alias feature/use-grid-for-transfer-rate-service-manager='nocorrect feature/use-grid-for-transfer-rate-service-manager'
-alias feature/rebooked-ride-reporting='nocorrect feature/rebooked-ride-reporting'
-alias gn='gn'
-
-# correct commands, but not any arguments (correct_all would do that)
-setopt correct
-alias sudo='nocorrect sudo'
+  alias man='nocorrect man'
+  alias mv='nocorrect mv'
+  alias mysql='nocorrect mysql'
+  alias mkdir='nocorrect mkdir'
+  alias gist='nocorrect gist'
+  alias heroku='nocorrect heroku'
+  alias ebuild='nocorrect ebuild'
+  alias hpodder='nocorrect hpodder'
+fi
