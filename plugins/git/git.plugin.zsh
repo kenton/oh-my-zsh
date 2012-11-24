baefd83078b5537412c2fc24989a5737d1636193
Stanislav Mekhonoshin <mehonoshin@sc.vsu.ru>
2012-11-24 11:23:44 +0400

    Added gwc(git whatchanged alias)
    Allows to view commit history in extended way

--- plugins/git/git.plugin.zsh
+++ plugins/git/git.plugin.zsh
@@ -58,11 +58,7 @@ alias gm='git merge'
 compdef _git gm=git-merge
 alias grh='git reset HEAD'
 alias grhh='git reset HEAD --hard'
-alias gpoat='git push origin --all && git push origin --tags'
-
-# Will cd into the top of the current repository
-# or submodule.
-alias grt='cd $(git rev-parse --show-toplevel || echo ".")'
+alias gwc='git whatchanged -p --abbrev-commit --pretty=medium'
 
 # Git and svn mix
 alias git-svn-dcommit-push='git svn dcommit && git push github master:svntrunk'
